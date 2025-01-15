35098f9e7d252993ea161d674bb106dbf0439900
AISHE Scott <scott.aishe@hexagon.com>
2025-01-14 21:56:37 -0500

    update

---
+++ index.js
@@ -0,0 +1,34 @@
+//create prompt
+const userInput = prompt("Enter Froyo flavors separated by commas:");
+
+//userInput --> vanilla,vanilla,vanilla,strawberry,coffee,coffee
+
+//spilt userInput using ,
+const order = userInput.split(",");
+console.log(userInput)
+console.log(order)
+
+//create function
+function orderQty(order) {
+  /** 
+    //create empty obj
+    //loop over array
+        //add the flavor as a key, with a value of 1 if the key doesn't exist otherwise add 1 to the current value
+            //ways to add 1 to current value obj[key] = obj[key]++ || obj[key]+=1
+
+    //return obj created above
+*/
+        let count = {};
+
+        for (const element of order) {
+            if (count[element]) {
+                count[element]++; 
+            }
+            else {
+                count[element] = 1; 
+            }
+        }
+}
+
+
+console.log(orderQty(order));
