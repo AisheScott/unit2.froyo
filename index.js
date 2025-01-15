ed833a2c536debca08d8c5605cd7247a1c5cea19
AISHE Scott <scott.aishe@hexagon.com>
2025-01-15 07:30:59 -0500

    count now working

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const userInput = prompt("Enter Froyo flavors separated by commas:");
 
 //spilt userInput using ,
 const order = userInput.split(",");
-console.log(userInput)
-console.log(order)
+//console.log(userInput)
+//console.log(order)
 
 //create function
 function orderQty(order) {
@@ -14,21 +14,22 @@ function orderQty(order) {
     //create empty obj
     //loop over array
         //add the flavor as a key, with a value of 1 if the key doesn't exist otherwise add 1 to the current value
-            //ways to add 1 to current value obj[key] = obj[key]++ || obj[key]+=1
+            //ways to add 1 to current value obj[key] = obj[key]++ || obj[key]+=1 ***key will be the flavor
 
     //return obj created above
 */
         let count = {};
 
-        for (const element of order) {
-            if (count[element]) {
-                count[element]++; 
+        for (const userInput of order) {
+            
+            if (count[userInput]) {
+                count[userInput]++; 
             }
             else {
-                count[element] = 1; 
+                count[userInput] = 1; 
             }
         }
+        return count;
 }
-
 
 console.log(orderQty(order));
